54cbe9c25b50ab77bec1c53213fc2223651b2a64
Tomás Moreira <tomaspintomoreira28@gmail.com>
2026-06-10 03:10:53 +0100

    feat: added compatibility to black play of bot.

--- main.py
+++ main.py
@@ -4,22 +4,32 @@ import chess
 
 from minimax import evaluate_positions
 
-if __name__ == "__main__":
+
+def start_game():
     sys.setrecursionlimit(10_000_000)
     board = chess.Board()
+    player_is_white = input("Are you going to play as white? (type w if so): ") == 'w'
+    is_player_turn = player_is_white
     while board.outcome() is None:
-        print("Bot playing...")
-        board.push(evaluate_positions(board.fen(), 7))
-        print(f"Bot played. Current FEN: {board.fen()}")
-        legal_moves = board.legal_moves
-        mv = input("Your move: ")
-        while True:
-            try:
-                legal = chess.Move.from_uci(mv) in legal_moves
-                if legal:
-                    board.push(chess.Move.from_uci(mv))
-                    break
-            except Exception as e:
-                print(e)
-                pass
-            mv = input("Illegal. Your move: ")
+        if is_player_turn:
+            legal_moves = board.legal_moves
+            mv = input("Your move: ")
+            while True:
+                try:
+                    legal = chess.Move.from_uci(mv) in legal_moves
+                    if legal:
+                        board.push(chess.Move.from_uci(mv))
+                        break
+                except Exception as e:
+                    print(e)
+                    pass
+                mv = input("Illegal. Your move: ")
+            is_player_turn = False
+        else:
+            print("Bot playing...")
+            board.push(evaluate_positions(board.fen(), 7, max_play_is_white=not player_is_white))
+            print(f"Bot played. Current FEN: {board.fen()}")
+            is_player_turn = True
+
+if __name__ == "__main__":
+    start_game()
--- minimax.py
+++ minimax.py
@@ -8,18 +8,18 @@ def heuristic(fen: str):
     return random() * 20 - 10
 
 
-def minimax_computation(legal_move, board_fen: str, depth=3, my_turn=True):
+def minimax_computation(legal_move, board_fen: str, depth=3, my_turn=True, max_play_is_white=True):
     if depth == 0:
         return heuristic(board_fen)
     board = chess.Board(fen=board_fen)
     board.push(legal_move)
-    res = minimax(board.fen(), depth=depth - 1, my_turn=not my_turn)
+    res = minimax(board.fen(), depth=depth - 1, my_turn=not my_turn, max_play_is_white=max_play_is_white)
     board.pop()
     del board
     return res
 
 
-def minimax(board_fen: str, depth=3, my_turn=True):
+def minimax(board_fen: str, depth=3, my_turn=True, max_play_is_white=True):
     board = chess.Board(fen=board_fen)
     legal = np.array(list(board.legal_moves))
     if depth == 0 or len(legal) == 0:
@@ -27,12 +27,12 @@ def minimax(board_fen: str, depth=3, my_turn=True):
     vectorized_computation = np.vectorize(minimax_computation)
     evaluations = vectorized_computation(legal, board.fen(), depth - 1, not my_turn)
     if my_turn:
-        return np.max(evaluations)
+        return np.max(evaluations) if max_play_is_white else np.min(evaluations)
     else:
-        return np.min(evaluations)
+        return np.min(evaluations) if max_play_is_white else np.max(evaluations)
 
 
-def evaluate_positions(board_fen: str, depth=3):
+def evaluate_positions(board_fen: str, depth=3, max_play_is_white=True):
     board = chess.Board(fen=board_fen)
     legal_moves = np.array(list(board.legal_moves))
     evaluations = []
@@ -40,4 +40,4 @@ def evaluate_positions(board_fen: str, depth=3):
     evaluations = vectorized_computation(
         legal_moves, board.fen(), depth - 1, my_turn=False
     )
-    return legal_moves[np.argmax(evaluations)]
+    return legal_moves[np.argmax(evaluations)] if max_play_is_white else legal_moves[np.argmin(evaluations)]
