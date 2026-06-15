b4780b113b9b0078364695ea0f2678132a967bca
Tomás Moreira <tomaspintomoreira28@gmail.com>
2026-06-15 13:28:52 +0100

    added more conv layers

--- heuristic_nn.py
+++ heuristic_nn.py
@@ -30,7 +30,13 @@ class ChessHeuristicEvaluator(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.heuristic = nn.Sequential(
-            nn.Conv2d(in_channels=12,out_channels=256, kernel_size=3, padding=1, stride=1),
+            nn.Conv2d(in_channels=12,out_channels=128, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=256),
+            nn.Conv2d(in_channels=128,out_channels=256, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=256),
+            nn.Conv2d(in_channels=256,out_channels=256, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=256),
             nn.Conv2d(in_channels=256,out_channels=256, kernel_size=3, padding=1, stride=1),
@@ -41,8 +47,6 @@ class ChessHeuristicEvaluator(nn.Module):
             nn.BatchNorm2d(num_features=256),
             nn.Flatten(),
             nn.Linear(256 * 8 * 8, 256),
-            nn.ReLU(),
-            nn.Linear(256, 256),
             nn.ReLU(),
             nn.Linear(256, 1)
         )
