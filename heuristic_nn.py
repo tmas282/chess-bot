d3ac6524c01d36b3a1f5569e9f2f9285d5f64080
Tomás Moreira <tomaspintomoreira28@gmail.com>
2026-06-15 14:13:00 +0100

    fix: batchnorm before relu, less neurons

--- heuristic_nn.py
+++ heuristic_nn.py
@@ -30,30 +30,26 @@ class ChessHeuristicEvaluator(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.heuristic = nn.Sequential(
-            nn.Dropout2d(p=0.2),
             nn.Conv2d(in_channels=12,out_channels=128, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=128),
             nn.Conv2d(in_channels=128,out_channels=256, kernel_size=3, padding=1, stride=1),
-            nn.ReLU(),
             nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
             nn.Conv2d(in_channels=256,out_channels=256, kernel_size=3, padding=1, stride=1),
-            nn.ReLU(),
             nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
             nn.Conv2d(in_channels=256,out_channels=256, kernel_size=3, padding=1, stride=1),
-            nn.ReLU(),
             nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
             nn.Conv2d(in_channels=256,out_channels=256, kernel_size=3, padding=1, stride=1),
-            nn.ReLU(),
             nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.Dropout(p=0.1),
             nn.Flatten(),
-            nn.Linear(256 * 8 * 8, 8192),
+            nn.Linear(256 * 8 * 8, 64),
             nn.ReLU(),
-            nn.Linear(8192, 2048),
-            nn.ReLU(),
-            nn.Linear(2048, 512),
-            nn.ReLU(),
-            nn.Linear(512, 1),
+            nn.Linear(64, 1),
         )
     def forward(self, x):
         x = x.type(torch.float32)
