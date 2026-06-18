ac310c6f4bafb31d160f81ae4910d6a047e29cca
Tomás Moreira <tomaspintomoreira28@gmail.com>
2026-06-18 15:26:31 +0100

    fix: memory explosion loading saved np array

--- utils/numpy_array_files.py
+++ utils/numpy_array_files.py
@@ -8,17 +8,37 @@ def get_saved_arrays(dataset_train_path: str, dataset_test_path: str, total: int
         y_test_list = []
         
         for i in range(total):
-            X_train_list.append(np.load(f"{dataset_train_path}_X_{i}.npy", allow_pickle=True))
-            X_test_list.append(np.load(f"{dataset_test_path}_X_{i}.npy", allow_pickle=True))
-            y_train_list.append(np.load(f"{dataset_train_path}_y_{i}.npy", allow_pickle=True))
-            y_test_list.append(np.load(f"{dataset_test_path}_y_{i}.npy", allow_pickle=True))
+            X_train_i = np.load(f"{dataset_train_path}_X_{i}.npy", allow_pickle=True)
+            X_test_i = np.load(f"{dataset_test_path}_X_{i}.npy", allow_pickle=True)
+            y_train_i = np.load(f"{dataset_train_path}_y_{i}.npy", allow_pickle=True)
+            y_test_i = np.load(f"{dataset_test_path}_y_{i}.npy", allow_pickle=True)
+            
+            X_train_list.append(X_train_i)
+            X_test_list.append(X_test_i)
+            
+            y_train_list.append(y_train_i)
+            y_test_list.append(y_test_i)
+            
+            del X_train_i.f
+            X_train_i.close()
+            del X_test_i.f
+            X_test_i.close()
+            
+            del y_train_i.f
+            y_train_i.close()
+            del y_test_i.f
+            y_test_i.close()
             
         print("All chunks loaded. Concatenating into final arrays...")
     
         X_train = np.concatenate(X_train_list, axis=0)
+        del X_train_list
         X_test = np.concatenate(X_test_list, axis=0)
+        del X_test_list
         y_train = np.concatenate(y_train_list, axis=0)
+        del y_train_list
         y_test = np.concatenate(y_test_list, axis=0)
+        del y_test_list
         
         return X_train, X_test, y_train, y_test
     except Exception as e:
